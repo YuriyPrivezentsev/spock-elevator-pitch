a3b0123255818edce6b3220227a153bf1140e535
YuriyPrivezentsev <y.privezentsev@gmail.com>
2019-09-11 21:33:01 +0200

    Add division

--- src/main/java/com/sabre/solvit/spock/elevator/calc/MathCalc.java
+++ src/main/java/com/sabre/solvit/spock/elevator/calc/MathCalc.java
@@ -1,9 +1,19 @@
 package com.sabre.solvit.spock.elevator.calc;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 class MathCalc {
     Double sum(Double ... operands){
-        return Arrays.stream(operands).mapToDouble(item -> item).sum();
+        return Arrays.stream(operands).filter(Objects::nonNull).mapToDouble(item -> item).sum();
     }
+
+    Double divide(Double numerator, Double denominator){
+        if(numerator == null || denominator == null || denominator == 0){
+            throw new IllegalArgumentException("Invalid fraction values");
+        }
+        return numerator/denominator;
+    }
+
 }
--- src/test/java/com/sabre/solvit/spock/elevator/calc/MathCalcTest.java
+++ src/test/java/com/sabre/solvit/spock/elevator/calc/MathCalcTest.java
@@ -1,6 +1,5 @@
 package com.sabre.solvit.spock.elevator.calc;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,8 +9,42 @@ class MathCalcTest {
     private MathCalc calculator = new MathCalc();
 
     @Test
-    void sum() {
+    void testSum() {
         Double sum = calculator.sum(2.5, 3.);
-        Assertions.assertEquals(5.5, sum.doubleValue());
+        assertEquals(5.5, sum.doubleValue());
     }
+
+    @Test
+    void testSumWithNull(){
+        Double sum = calculator.sum(null, 3.);
+        assertEquals(3., sum.doubleValue());
+    }
+
+    @Test
+    void testSumNullOnly(){
+        Double sum = calculator.sum(null, null);
+        assertEquals(0., sum.doubleValue());
+    }
+
+    @Test
+    void testDivision(){
+        Double fraction = calculator.divide(6., 2.);
+        assertEquals(3., fraction.doubleValue());
+    }
+
+    @Test
+    void testDivisionByZeroException(){
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(6., 0.));
+    }
+
+    @Test
+    void testDivisionByNullException(){
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(6., null));
+    }
+
+    @Test
+    void testDivisionOfNullException(){
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(null, 3.));
+    }
+
 }
